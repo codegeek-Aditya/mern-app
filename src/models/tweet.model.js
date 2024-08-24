78d35670085a3d7f154c852eca9c11c34a1f3fa4
codegeek-aditya <myidisadityasinha@gmail.com>
2024-08-24 14:38:15 +0530

    Add comment, like, playlist, tweet models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const tweetSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+export const Tweet = mongoose.model("Tweet", tweetSchema)
